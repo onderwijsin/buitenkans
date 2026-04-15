36406be2d3156fd9f3271ed62a9b1953d69bc58f
Remi Huigen <remihuigen@proton.me>
2026-04-15 22:49:58 +0200

    chore: Add gateway key as runtime config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -245,6 +245,7 @@ export default defineNuxtConfig({
 
 	runtimeConfig: {
 		apiToken: process.env.API_TOKEN,
+		aiGatewayApiKey: process.env.AI_GATEWAY_API_KEY,
 		cloudflare: {
 			accountId: process.env.CLOUDFLARE_ACCOUNT_ID,
 			apiToken: process.env.CLOUDFLARE_API_TOKEN,
