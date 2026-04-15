1789f5ea7116d2ab2ae68a787188a8c144c7def3
Remi Huigen <remihuigen@proton.me>
2026-04-15 19:29:01 +0200

    fix(ai): Potentially fix mcp transport errors in worker

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,34 +1,15 @@
-<script setup lang="ts">
-import { useElementSize } from '@vueuse/core'
-
-const containerRef = ref<HTMLElement | null>(null)
-
-// reactive height
-const { height } = useElementSize(containerRef)
-
-// tweak this constant to match your desired feel
-const SPEED_PER_PIXEL = 50
-
-// computed animation speed
-const speed = computed<number>(() => {
-	if (!height.value) return 75000 // fallback
-	return height.value * SPEED_PER_PIXEL
-})
-</script>
-
 <template>
 	<UMain>
 		<PatternBackground
-			ref="containerRef"
-			class="flex h-auto w-full flex-col items-center justify-center"
+			class="fixed! z-0 h-screen w-screen"
 			animate
 			direction="bottom"
 			variant="dot"
 			mask="ellipse-top"
 			size="md"
-			:speed="speed"
+			:speed="25000"
 		>
-			<slot />
 		</PatternBackground>
+		<slot />
 	</UMain>
 </template>
