6e3c955ee3e832753683183460b3dfd28144c714
Remi Huigen <remihuigen@proton.me>
2026-04-16 10:21:52 +0200

    chore: Link people images

--- server/routes/assets/[...pathname].get.ts
+++ server/routes/assets/[...pathname].get.ts
@@ -2,6 +2,7 @@ import { createError, eventHandler, getRouterParam } from 'h3'
 import { blob } from 'hub:blob'
 
 export default eventHandler(async (event) => {
+	console.log('hit')
 	const pathname = getRouterParam(event, 'pathname')
 
 	if (!pathname || /(?:^|[\\/])\.\.(?:[\\/]|$)/.test(pathname)) {
@@ -13,5 +14,6 @@ export default eventHandler(async (event) => {
 	 * is an assets dir. Since pathname no longer contains '/assets' here, we need to prepend it!
 	 */
 	setHeader(event, 'Content-Security-Policy', "default-src 'none';")
+	console.log(`/assets/${pathname}`)
 	return blob.serve(event, `/assets/${pathname}`)
 })
